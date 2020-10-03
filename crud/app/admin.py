7b6c0844d555f02801bdb0ec3f3e80073c83953c
Gaurav Tyagi <gktyagi2018@gmail.com>
2020-10-03 13:13:09 +0530

    first commit

---
+++ crud/app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+#from .models.position import Position
+#from .models.employee import Employee
+
+# Register your models here.
+#admin.site.register(Position)
+#admin.site.register(Employee)
